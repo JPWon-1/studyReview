619eee2a65760e1dd3cedfdbf78b05bac9b031ef
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-10-21 01:44:47 +0900

    봉우리 구하기

--- 자바알고리즘/Main.java
+++ 자바알고리즘/Main.java
@@ -1,31 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
+
+    int[] dx = { -1, 0, 1, 0 };
+    int[] dy = { 0, 1, 0, -1 };
+
     public static void main(String[] args) {
+        Main main = new Main();
+
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] arr = new int[n];
+        int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
         }
-        for (int i : solution(arr)) {
-            System.out.print(i + " ");
-        }
+
+        System.out.println(main.solution(n, arr));
+
     }
 
-    public static int[] solution(int[] arr) {
-        int length = arr.length;
-        int[] result = new int[length];
-        for (int i = 0; i < length; i++) {
-            int rank = 1;
-            for (int j = 0; j < length; j++) {
-                if (arr[i] < arr[j]) {
-                    rank++;
+    public int solution(int n, int[][] arr) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = i + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
                 }
+                if (flag)
+                    answer++;
             }
-            result[i] = rank;
         }
-        return result;
+        return answer;
     }
 
 }
