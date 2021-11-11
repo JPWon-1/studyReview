6c6ae7c34300bd9e3d7d4c99d5bb836c9287ed17
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-11-11 23:55:56 +0900

    save _config.yml (setting git.io)
    publish first md
    
    fix for github.io
    
    fix for github.io
    
    github io test
    
    test github io
    
    test github io
    
    test github io
    
    Revert "save _config.yml (setting git.io)"
    
    This reverts commit 283df4bea871d6b6b6a81863c72500dc00c65900.
    
    Revert "Revert "save _config.yml (setting git.io)""
    
    This reverts commit 050f8091d49251d0ba47f3d397bb18252a0a5b26.
    
    init github io
    
    github io init

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Test copy",
+        "excerpt":"test!  ","categories": [],
+        "tags": [],
+        "url": "/test-copy/",
+        "teaser": null
+      },{
+        "title": "Test",
+        "excerpt":"test!  ","categories": [],
+        "tags": [],
+        "url": "/test/",
+        "teaser": null
+      }]
