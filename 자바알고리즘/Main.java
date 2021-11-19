a8ab39b5d6a6788f72d7963e439f646745f89330
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-11-20 00:36:48 +0900

    자바 알고리즘 배열-멘토링

--- 자바알고리즘/Main.java
+++ 자바알고리즘/Main.java
@@ -6,36 +6,40 @@ public class Main {
         Main main = new Main();
 
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[][] arr = new int[n + 1][6];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 5; j++) {
+        int m = scanner.nextInt(); // 학생 수
+        int n = scanner.nextInt(); // 테스트 수
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = scanner.nextInt();
             }
         }
 
-        System.out.println(main.solution(n, arr));
+        System.out.println(main.solution(arr, m, n));
 
     }
 
-    public int solution(int n, int[][] arr) {
+    public int solution(int[][] arr, int memberCnt, int examCnt) {
         int answer = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= n; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= 5; k++) {
-                    if (arr[i][k] == arr[j][k]) {
-                        cnt++;
-                        break;
+        for (int i = 1; i <= memberCnt; i++) {
+            for (int j = 1; j <= memberCnt; j++) {
+                int tmpCnt = 0;
+                for (int k = 0; k < examCnt; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < memberCnt; s++) {
+                        if (arr[k][s] == i)
+                            pi = s;
+                        if (arr[k][s] == j)
+                            pj = s;
                     }
+                    if (pi < pj)
+                        tmpCnt++;
+                }
+                if (tmpCnt == examCnt) {
+                    answer++;
                 }
             }
-            if (cnt > max) {
-                max = cnt;
-                answer = i;
-            }
-
         }
 
         return answer;
